4b73d72d3e045127e479319441a2f6c33df76a66
Steven <stevenperasso@gmail.com>
2026-01-29 08:33:43 +0100

    fix6

--- javascript/main.js
+++ javascript/main.js
@@ -80,7 +80,7 @@ function generatePDF() {
 }
 
 function addSerial() {
-    let currentIndex = 0;
+    let currentIndex = 1;
     const input = document.getElementById("addingSerialInput");
     const serial = input.value.trim();
 
